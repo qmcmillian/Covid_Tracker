eb2abd0111a8568c41a2b357e3650d8365d58d98
Quentin McMillian <quentinmcmillian@gmail.com>
2021-05-01 19:44:48 -0400

    Setup App environment

---
+++ covid_tracker_app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Covid Application
+    </div>
+  );
+}
+
+export default App;
