aed4e4dec4410432648119af8bb6a7530fbe9bc8
Quentin McMillian <quentinmcmillian@gmail.com>
2021-05-03 19:47:45 -0400

    App successfully dipslays Covid Data on chart

--- covid_tracker_app/src/components/Chart.jsx
+++ covid_tracker_app/src/components/Chart.jsx
@@ -1,8 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { fetchDailyData } from '../helpers/apiHelpers';
+import {  Line, Bar } from 'react-chartjs-2';
+import { registerables } from 'chart.js';
+import '../styles/Chart.css';
 
 const Chart = ()  => {
+  const [dailyData, setDailyData] = useState([]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      const response = await fetchDailyData();
+      console.log('useeffect',response)
+      setDailyData(response);
+    }
+
+    fetchAPI();
+  }, []);
+
+  const lineChart =  (
+    dailyData.length
+      ? (
+        <Line
+          data={{
+            labels: dailyData.map( data => data.date), // returns all the dates
+            datasets: [
+              {
+                data: dailyData.map( data => data.confirmed),
+                label: 'Infected',
+                borderColor: '#3333ff',
+                fill: true,
+              },
+              {
+                data: dailyData.map( data => data.deaths),
+                label: 'Infected',
+                borderColor: 'red',
+                backgroundColor: 'rgba(255 , 0, 0, 0.5)',
+                fill: true
+              }
+          ],
+          }}
+        />) : null
+  );
+
   return (
-    <h1>Chart</h1>
+    <div className="container">
+      {lineChart}
+    </div>
   )
 }
 
